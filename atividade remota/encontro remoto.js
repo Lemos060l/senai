539731b07a53357c0f3c52bed13d73a2c134caef
matheus lemos <lemosm060@gmail.com>
2022-02-22 19:39:33 -0300

    atividade remota agr vai

---
+++ atividade remota/encontro remoto.js
@@ -0,0 +1,17 @@
+let numerodealuno   = 10; //vetor ou array
+
+
+for (let contador = 0; contador  <= numerodealuno; contador++) {
+
+    if (contador === 0) {
+
+        console.log("o número e igual a zero")
+    } else if (contador % 2 == 0 ) {
+
+    console.log("o número "+contador+ " é par")
+
+    } else {
+        console.log(`o número ${contador} é impar`) }
+
+
+}
