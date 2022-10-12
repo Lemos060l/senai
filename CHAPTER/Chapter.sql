a6667ac903e37a9c728614a47a458302b0d6691f
matheus lemos <lemosm060@gmail.com>
2022-10-11 21:15:26 -0300

    ER!

---
+++ CHAPTER/Chapter.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE Chapter2;USE Chapter2;CREATE TABLE Livros (	Id INT PRIMARY KEY IDENTITY,	Titulo VARCHAR(150) NOT NULL,	QuantidadePaginas INT,	Disponivel BIT);INSERT INTO Livros VALUES ('Titulo A', 120, 1);INSERT INTO Livros VALUES ('Titulo B', 220, 0);SELECT * FROM Livros;
+
+
+
+
+
+
