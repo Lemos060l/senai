5e1f2167edb17ab0b799e9a8b71c0732f1fbfe22
matheus lemos <lemosm060@gmail.com>
2022-10-18 20:23:58 -0300

    ER2

--- CHAPTER/Chapter/Chapter/Controllers/LivroController.cs
+++ CHAPTER/Chapter/Chapter/Controllers/LivroController.cs
@@ -1,4 +1,5 @@
-﻿using Chapter.Repositories;
+﻿using Chapter.Models;
+using Chapter.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,5 +29,75 @@ namespace Chapter.Controllers
                 throw new Exception(e.Message);
             }
         }
+
+        [HttpGet("{id}")]
+        public IActionResult buscarPorID(int id)
+        {
+            try
+            {
+                Livro livrobuscado = _livroRepository.buscarPorId(id);
+
+                if (livrobuscado == null)
+                {
+                    return NotFound();
+                }
+                return Ok(livrobuscado);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPost]
+
+        public IActionResult Cadastrar(Livro livro) 
+        {
+            try
+            {
+                _livroRepository.Cadastrar(livro);
+
+                return Ok("Livro cadastrado com sucesso");
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+
+                return Ok("Livro removido com sucesso");
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult Atualizar(int id, Livro livro)
+        {
+            try
+            {
+                _livroRepository.Atualizar(id, livro);
+
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
     }
 }
